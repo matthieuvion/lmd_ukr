ac4d3021adbd45737ed0f0ad4dcaf7cb39b7c4e2
matthieuvion <vionmatthieu@gmail.com>
2023-03-06 18:25:15 +0100

    added Comments css selectors

--- lmd_ukr/enums.py
+++ lmd_ukr/enums.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 
-class Selectors(Enum):
+class Css(Enum):
     # search html selectors
     S_IS_RESULT = "p.search__no-result"
     S_RIVER = "a.river__pagination"
@@ -14,3 +14,11 @@ class Selectors(Enum):
     A_DESC = "p.article__desc"
     A_METADATA = "script"
     A_CONTENT = "p.article__paragraph "
+
+    # comments html selectors
+    C_IS_COMMENT = "h3.comments__title"
+    C_COUNT = "h3.comments__title"
+    C_RIVER = "ul.pagination__list"
+    C_PAGES = "a.pagination__link"
+    C_AUTHOR = "span.comment__author"
+    C_CONTENT = "p.comment__content"
